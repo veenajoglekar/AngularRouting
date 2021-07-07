31e52b8bba2b18fe6dc1b2c802ef1018709a9d4c
veena1695 <veena1695@gmail.com>
2021-07-07 21:34:06 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,7 +8,12 @@ import { ServersComponent } from './servers/servers.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { UsersComponent } from './users/users.component';
+import { FormsModule } from '@angular/forms';
+import { ServersService } from './servers/servers.service'
 
+const appRoutes: Routes = [
+  { path: 'users', component: UsersComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,9 +25,10 @@ import { UsersComponent } from './users/users.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
